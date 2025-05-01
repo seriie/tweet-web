5d186eed69bb5ba8b52badee46c43b4dd1a80542
serrie <mohammadzidane058@gmail.com>
2025-05-01 23:03:52 +0700

    update dropdown

--- template/footer.php
+++ template/footer.php
@@ -1,10 +1,19 @@
 <script>
     const image = document.querySelector('.profile-img');
     const dropdown = document.querySelector('.profile-dropdown');
+    const dropdownContent = document.querySelectorAll('.profile-dropdown .content');
 
     image.addEventListener('click', function() {
-        dropdown.classList.remove('invisible');
+        dropdown.classList.toggle('invisible');
+    });
+
+    dropdownContent.forEach(element => {
+        element.addEventListener('click', function() {
+            dropdown.classList.toggle('invisible');
+        });
     });
 </script>
+
 </body>
+
 </html>
